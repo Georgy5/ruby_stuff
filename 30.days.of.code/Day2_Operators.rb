f39de96964003c6aa8eeff4576df9fea89427b11
Georgy5 <kgk86@hotmail.com>
2016-12-17 12:02:54 +0100

    Refactored Day2 code

--- 30.days.of.code/Day2_Operators.rb
+++ 30.days.of.code/Day2_Operators.rb
@@ -3,4 +3,4 @@ tipPercent = gets.chomp.to_i
 taxPercent = gets.chomp.to_i
 
 totalCost = mealCost + (mealCost*(tipPercent.to_f/100)) + (mealCost*(taxPercent.to_f/100))
-puts "The total meal cost is " + totalCost.round.to_s + " dollars."
+puts "The total meal cost is #{totalCost.round.to_s} dollars."
