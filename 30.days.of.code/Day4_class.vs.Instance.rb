53135ee6e5c70bab7db0277b96ab55a06fc934df
Georgy5 <kgk86@hotmail.com>
2016-12-17 12:04:53 +0100

    Refactored Day4 code

--- 30.days.of.code/Day4_class.vs.Instance.rb
+++ 30.days.of.code/Day4_class.vs.Instance.rb
@@ -1,16 +1,10 @@
 class Person
     attr_accessor :age
     def initialize(initialAge)
-        # Add some more code to run some checks on initialAge
-      if initialAge < 0
-        @age = 0
-        puts "Age is not valid, setting age to 0."
-      else
-        @age = initialAge
-      end   
+      @age = initialAge > 0 ? initialAge : 0
+      puts "Age is not valid, setting age to 0." if initialAge < 0
     end
     def amIOld()
-      # Do some computations in here and print out the correct statement to the console
       if (@age < 13)
         puts "You are young."
       elsif (13...18).include?(@age)
@@ -20,7 +14,6 @@ class Person
       end
     end
     def yearPasses()
-      # Increment the age of the person in here
       @age += 1
     end
 end
