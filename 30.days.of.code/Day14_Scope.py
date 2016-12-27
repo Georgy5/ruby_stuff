61b8bf9ec6dd6d1d71c6c0b54ba183cd919bd546
Georgy5 <kgk86@hotmail.com>
2016-12-27 13:28:19 +0100

    Day 14 challenge, Sope - done in Python 3

---
+++ 30.days.of.code/Day14_Scope.py
@@ -0,0 +1,20 @@
+class Difference:
+	   def __init__(self, a):
+        self.__elements = a
+
+	# Add your code here
+    def computeDifference(self):
+        N = self.elements        
+        self.maximumDifference = max(N) - min(N)
+
+
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
