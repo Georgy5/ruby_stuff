0d898963411fea9d3c0c6fe0711e4aa2faec5626
Karl Keller <kgk86@hotmail.com>
2021-06-15 21:33:00 +0200

    Create #merge_arrays with return statement

--- merge_sorted_arrays.rb
+++ merge_sorted_arrays.rb
@@ -2,5 +2,9 @@
 my_array     = [3, 4, 6, 10, 11, 15]
 alices_array = [1, 5, 8, 12, 14, 19]
 
+def merge_arrays(ary1, ary2)
+  return (ary1 + ary2).sort
+end
+
 puts merge_arrays(my_array, alices_array)
 # Prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
