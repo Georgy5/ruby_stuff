a0cad43c13a981fc61b3b9a527290082123bc568
Karl Keller <kgk86@hotmail.com>
2021-06-15 21:22:56 +0200

    Interview Cake Weekly Problem #352

---
+++ merge_sorted_arrays.rb
@@ -0,0 +1,6 @@
+# Interview Cake Weekly Problem #352
+my_array     = [3, 4, 6, 10, 11, 15]
+alices_array = [1, 5, 8, 12, 14, 19]
+
+puts merge_arrays(my_array, alices_array)
+# Prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
