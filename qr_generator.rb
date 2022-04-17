f8d1fa0b17feb057ce8db3b74e015a634f328bb1
Karl Keller <kgk86@hotmail.com>
2022-04-17 12:07:15 +0200

    QR code generator using RQRCode gem

---
+++ qr_generator.rb
@@ -0,0 +1,14 @@
+require 'rqrcode'
+
+content = ARGV[0]
+color = ARGV[1] ? "##{ARGV[1]}" : 'black'
+
+qr_code = RQRCode::QRCode.new(content)
+
+png = qr_code.as_png(
+  color: color,
+  fill: "white",
+  size: 300
+)
+
+IO.binwrite("qr_code.png", png.to_s)
