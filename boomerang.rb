4bccc0ddefe07e206cf0659de5ce90dbe597ace6
Karl Keller <kgk86@hotmail.com>
2023-05-20 00:00:04 +0200

    Add Boomerang code challenge

---
+++ boomerang.rb
@@ -0,0 +1,47 @@
+# array of numbers
+# Create a function to count boomerangs
+# A boomerang is 3 lements where the first and last numbers are the same,
+# the middle number however is different.
+# eg  [1, 2, 1, 2] would be two boomerangs
+
+# thoughts:
+# iterate and compare groups of three numbers
+
+def find_boomerang(ary)
+  none_found_message = "This list does not contain any Boomerangs"
+  if ary.length < 3
+    return none_found_message
+  end
+
+  boomerang_counter = 0
+  boomerang_list = []
+  ary.each_with_index do |num, index|
+    boom_ary = []
+    if ary[index] == ary[index + 2] and ary[index] != ary[index + 1]
+      boomerang_counter += 1
+      boom_ary << ary[index]
+      boom_ary << ary[index + 1]
+      boom_ary << ary[index + 2]
+    end
+    boomerang_list << boom_ary unless boom_ary.empty?
+  end
+
+  if boomerang_counter == 0
+    return none_found_message
+  end
+  
+  return "#{boomerang_counter} Boomerang(s) found: #{boomerang_list}"
+end
+
+# Test cases to test the #find_boomerang function
+test1 = [1, 2, 1, 2, 3, 4, 5, 6, 5]
+test2 = [1, 2, 1, 2]
+test3 = [2, 2]
+test4 = []
+test5 = [5, 5, 5, 5, 5]
+
+p find_boomerang(test1)
+p find_boomerang(test2)
+p find_boomerang(test3)
+p find_boomerang(test4)
+p find_boomerang(test5)
