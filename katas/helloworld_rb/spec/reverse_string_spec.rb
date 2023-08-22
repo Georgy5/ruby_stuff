87ae678a83fc9deb379141c609726baa207aa68e
Karl Keller <kgk86@hotmail.com>
2023-08-22 19:19:33 +0200

    Add test cases for invalid inputs

--- katas/helloworld_rb/spec/reverse_string_spec.rb
+++ katas/helloworld_rb/spec/reverse_string_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe 'reverse_string' do
   it 'reverses a longer string' do
     expect(reverse_string('programming')).to eq('gnimmargorp')
   end
+
+  it 'reverses a longer string' do
+    expect(reverse_string('programming')).to eq('gnimmargorp')
+  end
+
+  context 'with invalid input' do
+    it 'handles nil input' do
+      expect { reverse_string(nil) }.to raise_error(ArgumentError)
+    end
+
+    it 'handles non-string input' do
+      expect { reverse_string(123) }.to raise_error(ArgumentError)
+    end
+  end
 end
